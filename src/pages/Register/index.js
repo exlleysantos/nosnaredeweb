c1549d8c41f535d14eb325c5b97c67d6be7736a2
Exlley Santos <exlleyclemente@gmail.com>
2021-02-01 16:23:22 -0300

    Register Page

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -4,7 +4,7 @@ import * as Yup from 'yup';
 import { H1 } from '../../components/Typography';
 import Box from '../../components/Box';
 import Button from '../../components/Button';
-import { Form, Input, Select, TextArea, FileInput, MaskedInput } from '../../components/Form';
+import { Form, Input, Select, FileInput, MaskedInput } from '../../components/Form';
 import { Container, FormContainer, Title, Subjects, Subject, AvatarText } from './styles';
 
 import { GrFormAdd } from 'react-icons/gr';
@@ -14,22 +14,22 @@ import { useCallback } from 'react';
 import api from '../../services/api';
 
 const options = [
-	{ value: 1, label: 'Ensino fundamental' },
-	{ value: 2, label: 'Ensino médio' },
-	{ value: 3, label: 'Ensino superior' },
+	{ value: 'elementary_1', label: 'Ensino fundamental' },
+	{ value: 'elementary_2', label: 'Ensino médio' },
+	{ value: 'elementary_1', label: 'Ensino superior' },
 ];
 
 const Register = () => {
 	const formRef = useRef(null);
 
 	const [subjects, setSubjects] = useState([]);
-	const [teachingDegree, setTeachingDegree] = useState(undefined);
-
+	const [level, setTeachingDegree] = useState(undefined);
+	
 	const handleAddSubject = useCallback(() => {
 		const subject = formRef.current.getFieldValue('subjectName');
 
 		if (!subject) {
-			return formRef.current.setFieldError('subjectName', 'Informe o nome da disciplina');
+			return formRef.current.setFieldError('subjectName', 'Informe um interesse');
 		}
 
 		setSubjects((subjects) => [...subjects, subject]);
@@ -43,17 +43,17 @@ const Register = () => {
 	const handleSubmit = useCallback(
 		async (formData) => {
 			try {
-				console.log(formData);
+				console.log('isso é o formdata', formData);
 
 				const validationSchema = Yup.object().shape({
-					name: Yup.string().required('Informe seu nome'),
+					fullname: Yup.string().required('Informe seu nome'),
 					cpf: Yup.string().required('Informe seu SPF'),
 					birthDate: Yup.string().required('Informe sua data de nascimento'),
-					instituition: Yup.string().required('Campo obrigatório'),
-					teachingDegree: Yup.string().required('Campo obrigatório'),
-					tags: Yup.string().required('Campo obrigatório'),
-					nickname: Yup.string().required('Campo obrigatório'),
+					institution: Yup.string().required('Campo obrigatório'),
+					level: Yup.string().required('Campo obrigatório'),
+					username: Yup.string().required('Campo obrigatório'),
 					email: Yup.string().required('Campo obrigatório'),
+					password: Yup.string().required('Informe uma senha')
 				});
 
 				await validationSchema.validate(formData, { abortEarly: false });
@@ -61,23 +61,27 @@ const Register = () => {
 				formRef.current.setErrors({});
 
 				if (!subjects.length) {
-					alert('Informe suas disciplinas');
-					return formRef.current.setFieldError('subjectName', 'Informe suas disciplinas');
+					alert('Informe seus interesses');
+					return formRef.current.setFieldError('subjectName', 'Informe seus interesses');
 				}
 
 				const payload = new FormData();
-
+				
 				for (const key in formData) {
 					payload.append(key, formData[key]);
 				}
 
 				payload.append('subjects', subjects);
 
-				return console.log(payload.getAll());
+				//console.log('getAll', payload.getAll());
+				await api.post('users', payload);
+				return alert('USUÁRIO CADASTRADO!');
 			} catch (error) {
-				console.error(error);
+
+				console.error("aqui o seu erro, ó:", error.message);
 
 				if (error instanceof Yup.ValidationError) {
+					console.log('entrou aqui')
 					const errors = {};
 
 					error.inner.forEach(({ path, message }) => (errors[path] = message));
@@ -90,7 +94,7 @@ const Register = () => {
 
 	return (
 		<Container>
-			<Title onClick={() => console.log(teachingDegree)}>
+			<Title onClick={() => console.log(level)}>
 				#Entre<span>NÓS</span>naRede
 			</Title>
 			<FormContainer>
@@ -101,7 +105,7 @@ const Register = () => {
 						<h3>Dados pessoais</h3>
 
 						<Box>
-							<Input name='name' placeholder='Nome completo' size={2} />
+							<Input name='fullname' placeholder='Nome completo' size={2} />
 							<MaskedInput
 								name='cpf'
 								placeholder='CPF'
@@ -122,30 +126,29 @@ const Register = () => {
 
 						<Box>
 							<Input
-								name='instituition'
+								name='institution'
 								placeholder='Instituição de trabalho'
 								size={2}
 							/>
 							<Select
-								name='teachingDegree'
+								name='level'
 								options={options}
 								onChange={({ value }) => setTeachingDegree(value)}
 								placeholder='Grau de ensino'
 								size={2}
 							/>
 
-							{teachingDegree === 2 && (
+							{level === 2 && (
 								<Input name='series' placeholder='Ano/série' size={2} />
 							)}
 
 							<Input name='lattes' placeholder='Lattes (opcional)' size={1} />
 							<Input name='orcid' placeholder='Orcid (opcional)' size={1} />
-							<TextArea name='tags' rows={5} placeholder='Interesses' size={2} />
 
 							<Subjects>
-								<h3>Disciplinas que ministra</h3>
+								<h3>Interesses</h3>
 
-								<Input name='subjectName' placeholder='Nome da disciplina' />
+								<Input name='subjectName' placeholder='Ex: Matemática, Documentos' />
 								<Button type='button' isGhost onClick={handleAddSubject}>
 									<GrFormAdd />
 								</Button>
@@ -164,7 +167,7 @@ const Register = () => {
 										))
 									) : (
 										<li className='empty'>
-											Você ainda não adicionou uma disciplina
+											Você ainda não adicionou um interesse
 										</li>
 									)}
 								</ul>
@@ -176,8 +179,9 @@ const Register = () => {
 						<h3>Dados da conta</h3>
 
 						<Box>
-							<Input name='nickname' placeholder='Nome de usuário' size={2} />
+							<Input name='username' placeholder='Nome de usuário' size={2} />
 							<Input name='email' placeholder='Email' size={2} />
+							<Input name='password' placeholder='Senha' size={2} />
 
 							<AvatarText>
 								<h3>Foto de perfil</h3>

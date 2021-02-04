aaab29d16d87708fb24e7a880c6ccb1e91484bea
Exlley Santos <exlleyclemente@gmail.com>
2021-02-04 11:12:56 -0300

    configure services

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { getToken } from './auth';
 
-const api = axios.create({ baseURL: process.env.REACT_APP_API_URL });
+const api = axios.create({
+	 baseURL: process.env.REACT_APP_API_URL 
+});
 
 api.interceptors.request.use((config) => {
 	const token = getToken();
